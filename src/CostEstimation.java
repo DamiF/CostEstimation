73244696cfa38c79309c1837b3e18bf5f0145c20
DamiF <dalex_cu@yahoo.com>
2017-03-13 23:18:39 -0400

    Change and finish input and output sections
    Create a double array for material markup and a String array for
    material categories.
    Change input questions from console to dialog boxes.
    Code selection of material section.
    Finish the calculation considering all parameters.

--- src/CostEstimation.java
+++ src/CostEstimation.java
@@ -3,40 +3,54 @@
 //Class name: CostEstimation.java
 //This program performs a cost estimation.
 
-import java.util.Scanner;
+import javax.swing.*;
 
 public class CostEstimation {
 
 	public static void main(String[] args) {
 	
-		Scanner keyBoard=new Scanner(System.in);
 		double basePrice;
 		int person;
 		double flatMarkup;
 		double cost1=0;
+		double[] materialMarkup = {7.5, 13, 2, 0};
+		String[] materials = {"Pharmaceutical", "Food", "Electronics", "Other"};
+		double finalCost;
 		double cost2 = 0;
 		double cost3 = 0;
+		String choicesOptions = "";
+		int selectedMaterial;
+		
+		//----> Builds a string to be prompt to the user.
+
+		for(int x = 0; x < materialMarkup.length; ++x)
+			choicesOptions = choicesOptions + "\n" + (x+1) + "- " + materials[x];
 		
 		//----> Prompt questions to user.
 		
-		System.out.print("Enter the base price >> ");
-		basePrice=keyBoard.nextDouble();
+		basePrice = Double.parseDouble(JOptionPane.showInputDialog(null, "Enter the base price"));
+		person = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter how many person are involved"));
+		choicesOptions = JOptionPane.showInputDialog(null, "Please select one of the following materials: \n" + choicesOptions); 
+		selectedMaterial = Integer.parseInt(choicesOptions);
 				
-		System.out.print("Enter how many person are involved >> ");
-		person=keyBoard.nextInt();
-		
-		//Build material selection code
-		
 		//calculations
 		flatMarkup = basePrice * 5 / 100;
 		cost1 = basePrice + flatMarkup;
 		cost2 = (cost1 * 1.2/100 )*person;
 		
-		//calculate final cost depending on material selection
+		if (selectedMaterial != 4)
+		{
+			cost3 = cost1 * materialMarkup[selectedMaterial-1]/100;
+			finalCost = cost1 + cost2 + cost3;
+		}
+
+		else
+			finalCost = cost1 + cost2;
+		
 
 		//Output
 
-		System.out.print("The total cost is " + "$" + "finalCost");
+		System.out.print("The total cost is " + "$" + finalCost);
 		
 	}
 
