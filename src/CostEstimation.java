8179561313bcf4075df5d60eb5dd41adeb399ab0
DamiF <dalex_cu@yahoo.com>
2017-03-14 00:28:30 -0400

    Final touches
    Just organizing and cleaning the code view

--- src/CostEstimation.java
+++ src/CostEstimation.java
@@ -9,18 +9,18 @@ public class CostEstimation {
 
 	public static void main(String[] args) {
 	
-		double basePrice;
-		int person;
+		double basePrice = 0;
 		double flatMarkup;
-		double cost1=0;
 		double[] materialMarkup = {7.5, 13, 2, 0};
-		String[] materials = {"Pharmaceutical", "Food", "Electronics", "Other"};
 		double finalCost;
+		double cost1=0;
 		double cost2 = 0;
 		double cost3 = 0;
+		int person = 0;
+		int selectedMaterial = 0;
+		String[] materials = {"Pharmaceutical", "Food", "Electronics", "Other"};
 		String choicesOptions = "";
-		int selectedMaterial;
-		
+				
 		//----> Builds a string to be prompt to the user.
 
 		for(int x = 0; x < materialMarkup.length; ++x)
@@ -28,39 +28,33 @@ public class CostEstimation {
 		
 		//----> Prompt questions to user.
 		
-		try{
+		try{			
+			basePrice = Double.parseDouble(JOptionPane.showInputDialog(null, "Enter the base price"));
+			person = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter how many person are involved"));
+			choicesOptions = JOptionPane.showInputDialog(null, "Please select one of the following materials: \n" + choicesOptions); 
+			selectedMaterial = Integer.parseInt(choicesOptions);
+					
+			//calculations
+			flatMarkup = basePrice * 5 / 100;
+			cost1 = basePrice + flatMarkup;
+			cost2 = (cost1 * 1.2/100 )*person;
 			
-		basePrice = Double.parseDouble(JOptionPane.showInputDialog(null, "Enter the base price"));
-		person = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter how many person are involved"));
-		choicesOptions = JOptionPane.showInputDialog(null, "Please select one of the following materials: \n" + choicesOptions); 
-		selectedMaterial = Integer.parseInt(choicesOptions);
-				
-		//calculations
-		flatMarkup = basePrice * 5 / 100;
-		cost1 = basePrice + flatMarkup;
-		cost2 = (cost1 * 1.2/100 )*person;
-		
-		if (selectedMaterial != 4)
-		{
-			cost3 = cost1 * materialMarkup[selectedMaterial-1]/100;
-			finalCost = cost1 + cost2 + cost3;
+			if (selectedMaterial != 4){
+				cost3 = cost1 * materialMarkup[selectedMaterial-1]/100;
+				finalCost = cost1 + cost2 + cost3;
+			}
+
+			else
+				finalCost = cost1 + cost2;
+
+			//Output
+
+			JOptionPane.showMessageDialog(null, "The total cost is " + "$" + finalCost);
+			
+			}
+			
+			catch(Exception mistake){
+				JOptionPane.showMessageDialog(null, "Your answer is not valid");
+			}
 		}
-
-		else
-			finalCost = cost1 + cost2;
-		
-
-		//Output
-
-		JOptionPane.showMessageDialog(null, "The total cost is " + "$" + finalCost);
-		
-		}
-		
-		catch(Exception mistake)
-		{
-			JOptionPane.showMessageDialog(null, "Your answer is not valid");
-		}
-		
-	}
-
 }
